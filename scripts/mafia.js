a3abeaf522596c6b678d5fb64cb52fd172d7a788
PokeWorldBW <pokeworlddp@gmail.com>
2018-06-11 03:29:42 -0400

    found problem

--- scripts/mafia.js
+++ scripts/mafia.js
@@ -1657,9 +1657,11 @@ function Mafia(mafiachan) {
         } else {
             // Try to start a game from queue
             if (this.state === "blank" && !mafia.needsUpdating && mafia.queueingEnabled && mafia.queue.length > 0) {
-                var info = mafia.queue.splice(0, 1);
-                sys.sendAll(info.theme);
-                this.startGame(info.name, info.theme);
+                var info = mafia.queue.splice(0, 1)[0];
+                sys.sendAll(info);
+                sys.sendAll(info[0]);
+                sys.sendAll(info[1])
+                this.startGame(info[0], info[1]);
             }
         }
     };
@@ -7910,7 +7912,7 @@ function Mafia(mafiachan) {
             if (!theme) {
                 msg(src, "No such theme!");
             } else {
-                mafia.queue.push({ name: srcname, theme: theme });
+                mafia.queue.push([srcname, theme]);
                 msgAll(nonFlashing(sys.name(src)) + " added " + casedtheme(theme) + " to the queue.");
                 //msgAll(nonFlashing(sys.name(src)) + " added " + casedtheme(theme) + " to the Mafia theme queue.", sachannel);
             }
@@ -7940,7 +7942,7 @@ function Mafia(mafiachan) {
             } else {
                 for (var i = 0; i < mafia.queue.length; i++) {
                     var q = mafia.queue[i];
-                    if (q.theme === theme) {
+                    if (q[1] === theme) {
                         var t = mafia.queue.splice(i, 1);
                         msgAll(nonFlashing(sys.name(src)) + " removed " + casedtheme(t[1]) + " from the queue.");
                         //msgAll(nonFlashing(sys.name(src)) + " removed " + casedtheme(t[1]) + " from the Mafia theme queue.", sachannel);
