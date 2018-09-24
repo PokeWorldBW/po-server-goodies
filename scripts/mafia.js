9357ffdf363a8402e9e9c68709ee74f390535498
PokeWorldBW <pokeworlddp@gmail.com>
2018-09-24 03:40:16 -0400

    empty chargesmsg

--- scripts/mafia.js
+++ scripts/mafia.js
@@ -1038,7 +1038,7 @@ function Mafia(mafiachan) {
                                 abilities += " bypassing the modes " + readable(ability.bypass, "and");
                             }
                             if ("avoidHax" in role.actions && role.actions.avoidHax.indexOf(a) != -1) {
-                                abilities += "(Cannot be detected by spies) ";
+                                abilities += " (Cannot be detected by spies)";
                             }
                             abilities += ". ";
                         }
@@ -3731,7 +3731,7 @@ function Mafia(mafiachan) {
                     return true;
                 }
                 var charge = mafia.getCharges(player, "standby", commandName);
-                var chargetxt = (action.chargesmsg || "You have ~Charges~ charges remaining").replace(/~Charges~/g, charge);
+                var chargetxt = (chargesmsg in action ? action.chargesmsg : "You have ~Charges~ charges remaining").replace(/~Charges~/g, charge);
                 gamemsg(player.name, chargetxt);
             }
         };
@@ -4690,7 +4690,7 @@ function Mafia(mafiachan) {
                     }
                     if (mafia.getCharges(player, "night", o.action) !== undefined && targets.length > 0) {
                         var charge = mafia.getCharges(player, "night", o.action);
-                        var chargetxt = ( Action.chargesmsg || "You have ~Charges~ charges remaining").replace(/~Charges~/g, charge);
+                        var chargetxt = (chargesmsg in Action ?  Action.chargesmsg : "You have ~Charges~ charges remaining").replace(/~Charges~/g, charge);
                         gamemsg(player.name, chargetxt);
                     }
 
